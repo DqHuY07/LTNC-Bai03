b0013f9d6851cd7db05dc832167fa80f63fd6c4b
DqHuY07 <dinhquochuybh@gmail.com>
2026-05-05 22:12:00 +0700

    feat: add AppTest

---
+++ src/test/java/AppTest.java
@@ -0,0 +1,10 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class AppTest {
+  @Test
+  public void firstTest() {
+    assertEquals(App.add(10 , 10) , 10);
+  }
+}
